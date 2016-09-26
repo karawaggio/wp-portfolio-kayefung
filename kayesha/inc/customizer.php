d2629d81b73cf9affe98a6015633fcd78e1ee116
Arlene <arlene.ata@gmail.com>
2016-09-26 16:49:47 -0700

    Add files via upload

---
+++ kayesha/inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Kayesha Theme Customizer.
+ *
+ * @package Kayesha
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function kayesha_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'kayesha_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function kayesha_customize_preview_js() {
+	wp_enqueue_script( 'kayesha_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'kayesha_customize_preview_js' );
