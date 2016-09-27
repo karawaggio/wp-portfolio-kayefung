2421623325b53850c7e527a71d3d0f72181e01dd
karavaevsergey <sergeykaravaev44@gmail.com>
2016-09-27 16:20:35 -0700

    Header >> Fonts
    Added Opens Sans and Dosis

--- kayesha/header.php
+++ kayesha/header.php
@@ -15,6 +15,8 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
+<link href="https://fonts.googleapis.com/css?family=Dosis:400,700|Open+Sans:400,700" rel="stylesheet">
+
 
 <?php wp_head(); ?>
 </head>
