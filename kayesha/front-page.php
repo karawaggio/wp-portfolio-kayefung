94ed2341f509a56f8c5e71e9c01faf5fd5be754b
ArleneAta <arlene.ata@gmail.com>
2016-09-27 23:28:53 -0700

    home page
    display items, need to style and edit more

--- kayesha/front-page.php
+++ kayesha/front-page.php
@@ -18,9 +18,13 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
 			<?php
+
+
+
 			while ( have_posts() ) : the_post();
+				the_post_thumbnail('medium');
 				get_template_part( 'template-parts/content', 'page' );
-			endwhile; // End of the loop.
+		
 			?>
 
 	<?php //while ( have_posts() ) : the_post(); ?>
@@ -33,7 +37,13 @@ get_header(); ?>
 		              if (function_exists('get_field')){
 
 		                if(get_field('about')){
-		                the_field('about');
+		                	echo '<a href="';	
+			                the_field('about_feature_image');
+			           		echo '"><img src="';
+			               	the_field('about_feature_image');
+			               	echo '"></a>;';     
+			                the_field('about');
+
 		                    }
 		                }?>
 		            </section><!--.about  -->
@@ -43,13 +53,72 @@ get_header(); ?>
 		                 <?php  if (function_exists('get_field')){
 
 		                if(get_field('services')){
-		                the_field('services');
+
+		                		the_field('services_feature_image');
+		                	echo '<a href="';
+			               	the_field('services_feature_image');
+			               	echo '"><img src="';
+			               	the_field('services_feature_image');
+			               	echo '"></a>;';
+			                the_field('services');
 		                    }
 		                } ?>
 		            </section><!--.servcices  -->
 	           
 
             </section><!--.home-page  -->
+
+            <section class="front-slider">
+                <h2>Compliments</h2>
+
+                <div class="slider">
+                  <div class="flexslider">
+                    <ul class="slides">
+                      
+                        <?php
+                          $args = array (
+                                          'post_type' => 'testimonial',
+                                          'posts_per_page' => -1
+                                           );
+
+                          $compliments = new WP_Query($args);
+
+                          if($compliments->have_posts()){
+                            while($compliments->have_posts()){
+                              $compliments->the_post();
+                                echo "<li>";
+                              //display fields, if exists
+                              if(function_exists('get_field')){
+                                echo "<blockquote><figure>";
+                                            
+                                if(get_field('compliment_content')){
+                                  //echo "<>"
+                                  the_field('compliment_content');
+                                }
+                              
+                                echo "<figcaption>";
+                                if(get_field('compliment_author')){
+                                  the_field('compliment_author');
+                                } 
+                                echo "</figcaption>";
+                                echo "</figure>";
+                                echo "</blockquote>";
+                              }
+                                echo " </li>";
+                            }
+
+                            wp_reset_postdata(); 
+                          }
+
+                          ?>
+                     
+                    </ul>
+                  </div><!-- .flexslider -->
+                </div><!-- .slider -->
+
+
+
+            </section>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
