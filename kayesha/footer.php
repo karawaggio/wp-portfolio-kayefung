0e58c99e8a4c638de1a81daeb19586d94346e802
karavaevsergey <sergeykaravaev44@gmail.com>
2016-09-30 20:43:42 -0700

    New Footer!

--- kayesha/footer.php
+++ kayesha/footer.php
@@ -15,21 +15,27 @@
 	<footer id="colophon" class="site-footer" role="contentinfo">
 
 		<div class="container">
-			<div class="footer-nav">
-		    	<nav id="footer-navigation" class="footer-navigation">
-		        	<?php wp_nav_menu( array( 'theme_location' => 'footer') ); ?>
-		        </nav>
-		                	
-				<nav id="social-navigation" class="social-navigation">
-			        <?php wp_nav_menu( array( 'theme_location' => 'socialmenu') ); ?>
-		        </nav>
-			</div><!-- end of div footer nav -->
+			<section class="inner-container">
 
-			<div class="site-info">
-				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'kayesha' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'kayesha' ), 'WordPress' ); ?></a>
-				<span class="sep"> | </span>
-				<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'kayesha' ), 'kayesha', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
-			</div><!-- .site-info -->
+				<div class="footer-nav-copyright">
+			    	<nav id="footer-navigation" class="footer-navigation">
+			        	<?php wp_nav_menu( array( 'theme_location' => 'footer') ); ?>
+			        </nav>
+
+			        <p class="footer-copyright">&copy; 2016 Kayefung Artistry. All rights reserved.</p>
+				</div><!-- end of .footer-nav-copyright -->
+
+				<div class="social-nav-info">
+					<nav id="footer-social-navigation" class="footer-social-navigation">
+						<?php wp_nav_menu( array( 'theme_location' => 'socialmenu') ); ?>
+				    </nav>
+			    </div><!-- end of .social-nav-info -->
+			</section><!-- end of .footer-inner-container -->
+
+			<div class="developers-info">
+				<p>Developed by:&nbsp;&nbsp;<a href="#">Arlene Ata</a>,<a href="#"> Greg Voth</a>, and <a href="#">Sergey Karavaev</a></p>
+			</div><!-- .developers-info -->
+
 		</div><!-- end of .container -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
