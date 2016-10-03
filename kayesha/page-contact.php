82d695b12f356f25a9ddde290d87a450b394378a
GregVoth <gregvoth@hotmail.com>
2016-10-02 22:58:30 -0700

    Contact page changes

--- kayesha/page-contact.php
+++ kayesha/page-contact.php
@@ -18,9 +18,14 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
 			<?php
+			echo "<div class='maincontact'>";
 			while ( have_posts() ) : the_post();
 
-				get_template_part( 'template-parts/content', 'page' );
+			echo "<div class='contactimage'>";
+			the_post_thumbnail('medium');
+			echo "</div>";
+
+				get_template_part( 'template-parts/content', 'notitle' );
 
 				// If comments are open or we have at least one comment, load up the comment template.
 				// if ( comments_open() || get_comments_number() ) :
@@ -31,6 +36,28 @@ get_header(); ?>
 			?>
 
 			<?php
+			echo "<div class='contactinfo'>";
+        	if (function_exists ('get_field')){
+            	if(get_field('phone_number')){
+            	
+            	echo "<h3 class'phone'> Phone </h3>";
+            	echo "<p>";
+                the_field('phone_number');
+                echo "</p>";	
+            	}
+            	if(get_field('email')){
+            	echo "<h3 class='email'> Email </h3>";
+            	echo "<p>";
+                the_field('email');
+                echo "</p>";
+                	
+            	}
+        	}
+        	echo "</div>";
+        	echo "</div>";
+    		?>
+
+			<?php
                 $args = array (
                     'post_type' => 'faq',
                     'posts_per_page' => -1
@@ -38,15 +65,21 @@ get_header(); ?>
 
     		$faq = new WP_Query($args);
 
-
+			echo "<div class='faq'>";
+			echo "<h2> FAQ </h2>";
+			echo "<ul>";
     		if($faq->have_posts()){
     		 while($faq->have_posts()){
     		  $faq->the_post();
-    		   echo "<li>";
+    		  
+    		   echo "<li><h3 class='question'>";
     		  the_title();
+    		  echo "</h3><span class='answer'>";
     		  the_content();
-    		   echo " </li>";
+    		   echo " </span></li>";
     		 }//end while
+    		 echo "</ul>";
+    		 echo "</div>";
     		 wp_reset_postdata(); 
     		}//end if                      
     		?>
