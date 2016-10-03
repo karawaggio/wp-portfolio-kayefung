904a644aa85a06ce2ff5541044b010b73a86a0cd
karavaevsergey <sergeykaravaev44@gmail.com>
2016-10-03 11:54:16 -0700

    Post thumbs on Blog Page

--- kayesha/template-parts/content.php
+++ kayesha/template-parts/content.php
@@ -27,18 +27,25 @@
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
+
+		<?php the_post_thumbnail('kayesha-thumbnail', array('class' => 'center'));?>
+
 		<?php
-			the_content( sprintf(
-				/* translators: %s: Name of current post. */
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'kayesha' ), array( 'span' => array( 'class' => array() ) ) ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
+
+			the_excerpt();
+
+			// the_content( sprintf(
+			// 	/* translators: %s: Name of current post. */
+			// 	wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'kayesha' ), array( 'span' => array( 'class' => array() ) ) ),
+			// 	the_title( '<span class="screen-reader-text">"', '"</span>', false )
+			// ) );
 
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'kayesha' ),
 				'after'  => '</div>',
 			) );
 		?>
+
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
