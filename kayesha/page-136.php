10f41537907325df1d3b440ebacd1fda8d129b14
ArleneAta <arlene.ata@gmail.com>
2016-10-03 23:29:37 -0700

    center services page image

--- kayesha/page-136.php
+++ kayesha/page-136.php
@@ -18,8 +18,11 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
 			<?php
+			echo '<div class="services-page-image">';
 			the_post_thumbnail('large');
+			echo '</div>';
 			while ( have_posts() ) : the_post();
+	
 
 				get_template_part( 'template-parts/content', 'page' );
 
