cd326030ea9e7093e6f4b0e49800615cfc40f39d
ArleneAta <arlene.ata@gmail.com>
2016-10-04 00:20:49 -0700

    content for index
    new template for index.php so content.php remains the same

--- kayesha/index.php
+++ kayesha/index.php
@@ -37,7 +37,7 @@ get_header(); ?>
 		<section class="blog-main-page">
 			<?php while ( have_posts() ) : the_post();
 
-				get_template_part( 'template-parts/content', get_post_format() );
+				get_template_part( 'template-parts/contentindex', get_post_format() );
 
 				endwhile;
 
---
+++ kayesha/template-parts/contentindex.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Template part for displaying posts.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Kayesha
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<div class="entry-content">
+		<?php the_post_thumbnail('medium', array('class' => 'center')); ?>
+
+		<header class="entry-header">
+			<?php
+			if ( is_single() ) :
+				the_title( '<h1 class="entry-title">', '</h1>' );
+			else :
+				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+			endif;
+
+			if ( 'post' === get_post_type() ) : ?>
+			<div class="entry-meta">
+				<?php kayesha_posted_on(); ?>
+			</div><!-- .entry-meta -->
+			<?php
+			endif; ?>
+		</header><!-- .entry-header -->
+
+		<?php 
+
+			the_excerpt(); 
+
+			// the_content( sprintf(
+			// 	/* translators: %s: Name of current post. */
+			// 	wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'kayesha' ), array( 'span' => array( 'class' => array() ) ) ),
+			// 	the_title( '<span class="screen-reader-text">"', '"</span>', false )
+			// ) );
+
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'kayesha' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php kayesha_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
