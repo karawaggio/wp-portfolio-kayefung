611e3290447dcccedd3b4147c1ff1c7d133e84e8
ArleneAta <arlene.ata@gmail.com>
2016-10-14 00:33:45 -0700

    added shortcode to contact page

--- kayesha/page-contact.php
+++ kayesha/page-contact.php
@@ -33,7 +33,7 @@ get_header(); ?>
 			the_post_thumbnail('medium');
 			echo "</div>";
 
-				get_template_part( 'template-parts/content', 'notitle' );
+				get_template_part( 'template-parts/content', 'contact' );
 
 				// If comments are open or we have at least one comment, load up the comment template.
 				// if ( comments_open() || get_comments_number() ) :
