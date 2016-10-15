06aed2905ae1abeaad2a3bd6b4f3e042e659cc08
karavaevsergey <sergeykaravaev44@gmail.com>
2016-10-15 14:42:28 -0700

    404 page

--- kayesha/404.php
+++ kayesha/404.php
@@ -10,51 +10,24 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+		<main id="main" class="site-main error-content" role="main">
 
 			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'kayesha' ); ?></h1>
-				</header><!-- .page-header -->
 
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'kayesha' ); ?></p>
+				<div class="page404-content">
+				<img class="kayefung-logo" src="<?php echo get_template_directory_uri(); ?>/images/kaye-small-logo-2.png" alt="" />
+					<h1 class="page404-title"><?php esc_html_e( '404: Sorry, the page you are looking for cannot be found.', 'kayesha' ); ?></h1>
 
-					<?php
-						get_search_form();
+					<ul>
+						<li><p class="page404-text">If you typed the URL directly, please check if the spelling is correct.</p></li>
+						<li><p class="page404-text">If you clicked on a link to get here, the content was moved to the new URL.</p></li>
+						<li><p class="page404-text">If you are not sure how you got here, simply return to the <a class="page404-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">Homepage.</a></p></li>
+						<li><p class="page404-text">Please try search box below to search for an item.</p></li>
+					</ul>
 
-						the_widget( 'WP_Widget_Recent_Posts' );
+					<?php get_search_form(); ?>
+				</div><!-- .error-content -->
 
-						// Only show the widget if site has multiple categories.
-						if ( kayesha_categorized_blog() ) :
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'kayesha' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-						endif;
-
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'kayesha' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-						the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
 
 		</main><!-- #main -->
