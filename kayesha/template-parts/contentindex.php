0a6b26c1813da88b1e924808aaa979671c1378ee
karavaevsergey <sergeykaravaev44@gmail.com>
2016-10-17 11:30:14 -0700

    Last Blog Change, I Promiseeee!

--- kayesha/template-parts/contentindex.php
+++ kayesha/template-parts/contentindex.php
@@ -10,49 +10,49 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-	<div class="entry-content">
-		<?php
-		echo "<div class='blogimagecontainer'>"; 
-		the_post_thumbnail('', array('class' => 'center'));
-		echo "</div>"; 
-		?>
-
-		<footer class="entry-footer">
-		<?php kayesha_entry_footer(); ?>
-		</footer><!-- .entry-footer -->
-
-		<header class="entry-header">
+	<section class="blogpage">
+		<div class="entry-content">
 			<?php
-			if ( is_single() ) :
-				the_title( '<h1 class="entry-title">', '</h1>' );
-			else :
-				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-			endif;
+			echo "<div class='blogimagecontainer'>"; 
+			the_post_thumbnail('', array('class' => 'center'));
+			echo "</div>"; 
+			?>
 
-			if ( 'post' === get_post_type() ) : ?>
-			<div class="entry-meta">
-				<?php kayesha_posted_on(); ?>
-			</div><!-- .entry-meta -->
-			<?php
-			endif; ?>
-		</header><!-- .entry-header -->
+			<footer class="entry-footer">
+			<?php kayesha_entry_footer(); ?>
+			</footer><!-- .entry-footer -->
 
-		<?php 
+			<header class="entry-header">
+				<?php
+				if ( is_single() ) :
+					the_title( '<h1 class="entry-title">', '</h1>' );
+				else :
+					the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+				endif;
 
-			the_excerpt(); 
+				if ( 'post' === get_post_type() ) : ?>
+				<div class="entry-meta">
+					<?php kayesha_posted_on(); ?>
+				</div><!-- .entry-meta -->
+				<?php
+				endif; ?>
+			</header><!-- .entry-header -->
 
-			// the_content( sprintf(
-			// 	/* translators: %s: Name of current post. */
-			// 	wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'kayesha' ), array( 'span' => array( 'class' => array() ) ) ),
-			// 	the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			// ) );
+			<?php 
 
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'kayesha' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
+				the_excerpt(); 
 
+				// the_content( sprintf(
+				// 	/* translators: %s: Name of current post. */
+				// 	wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'kayesha' ), array( 'span' => array( 'class' => array() ) ) ),
+				// 	the_title( '<span class="screen-reader-text">"', '"</span>', false )
+				// ) );
+
+				wp_link_pages( array(
+					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'kayesha' ),
+					'after'  => '</div>',
+				) );
+			?>
+		</div><!-- .entry-content -->
+	</section><!-- .blogpage -->
 </article><!-- #post-## -->
